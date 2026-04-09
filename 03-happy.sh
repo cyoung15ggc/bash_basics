4d5752552015129953194da7f4f72ae8cd0944bb
chris.young <cyoung15@ggc.edu>
2026-04-08 22:56:40 -0400

    Edited happy script on main

--- 03-happy.sh
+++ 03-happy.sh
@@ -7,7 +7,27 @@ if [ "$answer" = "yes" ]; then
    echo "hmmmm gooood yess you are happy"
 else
    echo "Still Smile c:"
+
 fi
+
+
+
+echo "Today is a good day"
+echo "I went to school"
+echo "code is cool"
+
+name="Chris Young"
+echo "Hello $name"
+
+echo "What is your favorite color?"
+read color
+echo "Your favorite color is $color"
+
+num=5
+echo $((num + 2))
+
+
+
 
 # here are the other string comparison operators
 # != , -n (not an empty string) , -z (an empty string)
